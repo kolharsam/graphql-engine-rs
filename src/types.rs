69d22ba4dc0598f23e52e9915120c08a35749e27
Sameer Kolhar <kolhar730@gmail.com>
2021-08-22 21:03:48 +0530

    add new FieldName type to help support/record aliases

--- src/types.rs
+++ src/types.rs
@@ -88,3 +88,52 @@ impl Metadata {
         Ok(())
     }
 }
+
+// FieldName is a type exclusively for GraphQL
+#[derive(Debug, Serialize, Clone, PartialEq, Eq, std::hash::Hash)]
+pub struct FieldName(
+    Option<String>, // this is for any alias
+    String,         // this is the actual field name
+);
+
+impl FieldName {
+    pub fn new(field_name: &str, field_alias: Option<String>) -> FieldName {
+        FieldName(field_alias, field_name.to_string())
+    }
+
+    pub fn to_sql(&self) -> String {
+        let mut alias = String::from(&self.1);
+        if let Some(a) = self.0.clone() {
+            alias = a;
+        }
+
+        format!("{} AS {}", utils::dquote(self.1.as_str()), alias)
+    }
+
+    pub fn name(&self) -> String {
+        self.1.clone()
+    }
+
+    pub fn alias(&self) -> String {
+        if let Some(alias) = self.0.clone() {
+            return alias;
+        }
+
+        self.1.clone()
+    }
+}
+
+#[test]
+fn field_name_to_sql_with_no_alias() {
+    let new_field_name = FieldName::new("users", None);
+    assert_eq!(new_field_name.to_sql(), "\"users\" AS users".to_string());
+}
+
+#[test]
+fn field_name_to_sql_with_alias() {
+    let new_field_name = FieldName::new("users", Some("new_users".to_string()));
+    assert_eq!(
+        new_field_name.to_sql(),
+        "\"users\" AS new_users".to_string()
+    );
+}
