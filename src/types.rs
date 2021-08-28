fabbeecb283492e49ea5add91beee2b169273128
Sameer Kolhar <kolhar730@gmail.com>
2021-08-28 12:49:01 +0530

    add new types to support arguments in query processing

--- src/types.rs
+++ src/types.rs
@@ -92,8 +92,8 @@ impl Metadata {
 // FieldName is a type exclusively for GraphQL
 #[derive(Debug, Serialize, Clone, PartialEq, Eq, std::hash::Hash)]
 pub struct FieldName(
-    Option<String>, // this is for any alias
-    String,         // this is the actual field name
+    pub Option<String>, // this is for any alias
+    pub String,         // this is the actual field name
 );
 
 impl FieldName {
@@ -123,6 +123,17 @@ impl FieldName {
     }
 }
 
+// Returns the string values of the `Field` names within a selection set
+pub fn field_names_to_name_list(nl: &[FieldName]) -> Vec<String> {
+    let mut names: Vec<String> = Vec::new();
+
+    for f_name in nl {
+        names.push(f_name.1.clone());
+    }
+
+    names
+}
+
 #[test]
 fn field_name_to_sql_with_no_alias() {
     let new_field_name = FieldName::new("users", None);
@@ -137,3 +148,99 @@ fn field_name_to_sql_with_alias() {
         "\"users\" AS new_users".to_string()
     );
 }
+
+// NOTE: GQLArgs is a simplified version of the
+// AST's representation of arguments. Using
+// such a structure only because it's easier
+// to do operations. The AST is much more
+// sophisticated and hence can become cumbersome
+#[derive(Serialize, Clone, Debug)]
+pub enum GQLArgType {
+    // NOTE: supported for [distinct_on]
+    String(String),
+    // NOTE: supported for [limit, offset]
+    Int(i64),
+    // Float(f32)
+}
+
+type FieldArguments = indexmap::IndexMap<String, GQLArgType>;
+
+impl GQLArgType {
+    // NOTE: functions like the one's defined below
+    // should be limited in usage since we can go
+    // wrong quite easily since we're relying on the
+    // developer's conscience over the type system
+    // hence opening up an avenue for bugs and issues
+
+    pub fn get_num(&self) -> i64 {
+        if let GQLArgType::Int(num) = self {
+            return *num;
+        }
+        // FIXME?: This should/would never happen
+        // unless we use it incorrectly
+        0
+    }
+
+    pub fn get_string(&self) -> String {
+        if let GQLArgType::String(txt) = &self {
+            return txt.to_string();
+        }
+        // FIXME?: This should/would never happen
+        // unless we use it incorrectly
+        "".to_string()
+    }
+}
+
+#[derive(Serialize, Clone, Debug)]
+pub struct FieldInfo {
+    pub fields: Vec<FieldName>,
+    pub root_field_arguments: indexmap::IndexMap<String, GQLArgType>,
+}
+
+impl FieldInfo {
+    pub fn new(fields: Vec<FieldName>, args: FieldArguments) -> FieldInfo {
+        FieldInfo {
+            fields,
+            root_field_arguments: args,
+        }
+    }
+}
+
+pub fn to_string_arg<'a>(
+    arg_name: String,
+    arg_val: &graphql_parser::query::Value<'a, &'a str>,
+) -> Result<(String, GQLArgType), error::GQLRSError> {
+    if let graphql_parser::query::Value::Enum(st) = arg_val {
+        return Ok((arg_name, GQLArgType::String(st.to_string())));
+    }
+
+    Err(error::GQLRSError::new(error::GQLRSErrorType::GenericError(
+        "failed to parse string".to_string(),
+    )))
+}
+
+pub fn to_int_arg<'a>(
+    arg_name: String,
+    arg_val: &graphql_parser::query::Value<'a, &'a str>,
+) -> Result<(String, GQLArgType), error::GQLRSError> {
+    if let graphql_parser::query::Value::Int(num) = arg_val {
+        let inum = num.as_i64();
+        match inum {
+            Some(n) => return Ok((arg_name, GQLArgType::Int(n))),
+            None => {
+                return Err(error::GQLRSError::new(error::GQLRSErrorType::GenericError(
+                    "int is overflown".to_string(),
+                )))
+            }
+        }
+    }
+
+    Err(error::GQLRSError::new(error::GQLRSErrorType::GenericError(
+        "failed to parse int".to_string(),
+    )))
+}
+
+// NOTE: these arguments are case sensitive, in case they're not
+// these exactly we have every right to reject the query!
+pub const SUPPORTED_STRING_GQL_ARGUMENTS: [&str; 1] = ["distinct_on"];
+pub const SUPPORTED_INT_GQL_ARGUMENTS: [&str; 2] = ["offset", "limit"];
