752ea886f46ae5667b68d838000009e9d2835f35
Sameer Kolhar <kolhar730@gmail.com>
2021-08-28 12:49:31 +0530

    add step to process query arguments

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,14 @@
+use indexmap::IndexMap;
 use postgres::types::Json;
 use postgres::{Client, Row};
 use serde::{Deserialize, Serialize};
 
 use crate::db;
 use crate::error;
-use crate::types::{FieldName, QualifiedTable};
+use crate::types::{
+    field_names_to_name_list, to_int_arg, to_string_arg, FieldInfo, FieldName, GQLArgType,
+    QualifiedTable, SUPPORTED_INT_GQL_ARGUMENTS, SUPPORTED_STRING_GQL_ARGUMENTS,
+};
 
 pub async fn healthz_handler(_req: actix_web::HttpRequest) -> String {
     "OK".to_string()
@@ -34,7 +38,7 @@ pub struct ErrorResponse {
 // NOTE: this should be used for sending the API response
 #[derive(Serialize, Debug, Clone)]
 pub struct DataResponse {
-    data: indexmap::IndexMap<String, serde_json::Value>,
+    data: IndexMap<String, serde_json::Value>,
 }
 
 pub async fn metadata_handler(payload: actix_web::web::Bytes) -> actix_web::HttpResponse {
@@ -54,7 +58,7 @@ pub async fn metadata_handler(payload: actix_web::web::Bytes) -> actix_web::Http
             .content_type("application/json")
             .body(
                 serde_json::to_string(&b)
-                    .unwrap_or("Failed to convert body to a valid string".to_string()),
+                    .unwrap_or_else(|_| "Failed to convert body to a valid string".to_string()),
             ),
         Err(e) => actix_web::HttpResponse::build(actix_web::http::StatusCode::BAD_REQUEST).json(
             ErrorResponse {
@@ -69,18 +73,18 @@ pub async fn metadata_handler(payload: actix_web::web::Bytes) -> actix_web::Http
     // })
 }
 
-fn empty_query_variables() -> indexmap::IndexMap<String, String> {
-    indexmap::IndexMap::new()
+fn empty_query_variables() -> IndexMap<String, String> {
+    IndexMap::new()
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct GraphQLRequest {
     query: String,
     #[serde(default = "empty_query_variables")]
-    variables: indexmap::IndexMap<String, String>,
+    variables: IndexMap<String, String>,
 }
 
-type GQLResult = indexmap::IndexMap<String, serde_json::Value>;
+type GQLResult = IndexMap<String, serde_json::Value>;
 
 fn fetch_result_from_query_fields<'a>(
     qry_sel_set: &graphql_parser::query::SelectionSet<'a, &'a str>,
@@ -89,27 +93,61 @@ fn fetch_result_from_query_fields<'a>(
     // many of the patterns, like Query, Selection Set and eventually Subscriptions!
     pg_client: &mut Client,
 ) -> actix_web::HttpResponse {
-    let mut fields_map: indexmap::IndexMap<FieldName, Vec<FieldName>> = indexmap::IndexMap::new();
+    let mut fields_map: IndexMap<FieldName, FieldInfo> = IndexMap::new();
 
     for set in qry_sel_set.items.iter() {
+        // NOTE: Nothing's being done for the other variants of the Selection enum
         if let graphql_parser::query::Selection::Field(field) = set {
             // FIXME: make this recursive too, this is just one level now...
             let table_name = field.name.to_string();
             let alias = field.alias.map(String::from);
             let root_field_name = FieldName::new(&table_name, alias);
-            fields_map.insert(
-                root_field_name,
-                selection_set_fields_parser(&field.selection_set),
-            );
+            let mut field_args: IndexMap<String, GQLArgType> = IndexMap::new();
+            let sub_fields = selection_set_fields_parser(&field.selection_set);
+
+            if !field.arguments.is_empty() {
+                for root_field_arg in field.arguments.iter() {
+                    let arg_name = root_field_arg.0.to_string();
+                    let arg_value = &root_field_arg.1;
+                    if SUPPORTED_STRING_GQL_ARGUMENTS.contains(&arg_name.as_str()) {
+                        let convert_to_string_arg = to_string_arg(arg_name, arg_value);
+                        if let Ok(fa) = convert_to_string_arg {
+                            let str_fields = field_names_to_name_list(&sub_fields);
+                            if str_fields.contains(&fa.1.get_string()) {
+                                field_args.insert(fa.0, fa.1);
+                            } else {
+                                return actix_web::HttpResponse::Ok().json(ErrorResponse {
+                                    error: format!("The value should be one of: {:?}", str_fields),
+                                });
+                            }
+                        } else if let Err(e) = convert_to_string_arg {
+                            return actix_web::HttpResponse::Ok().json(ErrorResponse {
+                                error: e.to_string(),
+                            });
+                        }
+                    } else if SUPPORTED_INT_GQL_ARGUMENTS.contains(&arg_name.as_str()) {
+                        let convert_to_int_arg = to_int_arg(arg_name, arg_value);
+                        if let Ok(fa) = convert_to_int_arg {
+                            field_args.insert(fa.0, fa.1);
+                        } else if let Err(e) = convert_to_int_arg {
+                            return actix_web::HttpResponse::Ok().json(ErrorResponse {
+                                error: e.to_string(),
+                            });
+                        }
+                    }
+                }
+            }
+
+            fields_map.insert(root_field_name, FieldInfo::new(sub_fields, field_args));
         }
     }
 
     let mut result_rows: Vec<Row> = Vec::new();
     for field_info in fields_map.iter() {
         let root_field_name = field_info.0;
-        let fields = field_info.1;
+        let fields_info_struct = field_info.1;
 
-        let query_res = db::get_rows_gql_query(pg_client, root_field_name, fields);
+        let query_res = db::get_rows_gql_query(pg_client, root_field_name, fields_info_struct);
         match query_res {
             Ok(db_res) => {
                 result_rows.push(db_res);
@@ -123,7 +161,7 @@ fn fetch_result_from_query_fields<'a>(
         }
     }
 
-    let mut final_res: GQLResult = indexmap::IndexMap::new();
+    let mut final_res: GQLResult = IndexMap::new();
 
     for res_row in result_rows.iter() {
         // FIXME: can be a potential point of failure
@@ -136,8 +174,6 @@ fn fetch_result_from_query_fields<'a>(
             }
             // NOTE: this error is reported when we encounter no rows or nulls
             Err(_err) => {
-                // FIXME: this is not consistent with Hasura's behavior, in Hasura,
-                // all of the columns in the query are placed with a `null`
                 final_res.insert(
                     root_field_name.to_string(),
                     serde_json::json!({ root_field_name.to_string(): serde_json::Value::Null }),
