469ff5520ee6d8036be900ed4f250d089d39b9f7
Sameer Kolhar <kolhar730@gmail.com>
2021-08-30 10:56:25 +0530

    remove unwanted utils

--- src/utils.rs
+++ src/utils.rs
@@ -12,14 +12,6 @@ pub fn to_camel_case(s: &str) -> String {
     s.to_case(Case::Camel)
 }
 
-pub fn to_upper_case(s: &str) -> String {
-    s.to_case(Case::Upper)
-}
-
-pub fn to_lower_case(s: &str) -> String {
-    s.to_case(Case::Lower)
-}
-
 // UNSAFE NOTICE: this option is very unsafe!! use with caution!
 pub fn string_to_static_str(s: String) -> &'static str {
     Box::leak(s.into_boxed_str())
