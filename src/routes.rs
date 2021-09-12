de28157aaf4982d3b9a984c60c9aec322337bb3c
iykekings <iykekings36gmail.com>
2021-09-12 14:41:41 +0100

    refactor out routes

--- src/routes.rs
+++ src/routes.rs
@@ -5,13 +5,6 @@ use crate::websocket;
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/healthz").route(web::get().to(server::healthz_handler)))
-        // TODO: make the version "v1" as one `resource` and then add these routes there
-        // .service(web::resource("/v1/metadata").route(web::post().to(server::metadata_handler)))
-        // .service(
-        //     web::resource("/v1/graphql")
-        //         .route(web::post().to(server::graphql_handler))
-        //         .route(web::get().to(websocket::ws_index)),
-        // )
         .service(
             web::scope("/v1")
                 .route("/metadata", web::post().to(server::metadata_handler))
