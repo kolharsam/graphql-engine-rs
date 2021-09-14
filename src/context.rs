3e4f34cb600d77e7693c2e3e4f4b6b5a35d7fd4f
Sameer Kolhar <kolhar730@gmail.com>
2021-09-14 13:05:54 +0530

    update context file for pool and status

--- src/context.rs
+++ src/context.rs
@@ -1,45 +1,44 @@
-use postgres::Client;
+use postgres::NoTls;
+use r2d2::Pool;
+use r2d2_postgres::PostgresConnectionManager;
+use serde::{Deserialize, Serialize};
 
-use crate::types;
-
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
 pub enum Status {
-    Healthy,
-    Errored,
+    Ok,
+    Error,
 }
 
+type PGPool = Pool<PostgresConnectionManager<NoTls>>;
+
+#[derive(Clone, Debug)]
 pub struct ServerCtx {
-    pub client: Client,
-    pub metadata: types::Metadata,
-    pub status: Status,
+    conn_pool: PGPool,
+    status: Status,
 }
 
 impl ServerCtx {
-    fn new(&self, pg_client: Client, metadata: types::Metadata) -> ServerCtx {
+    pub fn new(pg_pool: PGPool) -> ServerCtx {
         ServerCtx {
-            client: pg_client,
-            metadata,
-            status: Status::Healthy,
+            conn_pool: pg_pool,
+            status: Status::Ok,
         }
     }
 
-    fn get_status(&self) -> Status {
-        self.status
+    pub fn get_status_json(&self) -> serde_json::Value {
+        serde_json::json!({ "status": &self.status })
     }
 
-    fn set_status(&mut self, status: Status) {
-        self.status = status
+    fn set_status_to_errored(&mut self) {
+        self.status = Status::Error
     }
 
-    fn get_metadata(&self) -> &types::Metadata {
-        &self.metadata
+    fn set_status_to_healthy(&mut self) {
+        self.status = Status::Ok
     }
 
-    // fn set_metadata(&mut self, metadata: Metadata)
-    // TODO: -- This is not needed since metadata has all the methods
-    // but something to think about in terms of design
-
-    fn get_client(&self) -> &Client {
-        &self.client
+    pub fn get_connection_pool(&self) -> &PGPool {
+        &self.conn_pool
     }
 }
